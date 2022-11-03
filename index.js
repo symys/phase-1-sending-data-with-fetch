380971020ba143a5b255c065426c246a448f137e
SeymaDemir <seymademir35@gmail.com>
2022-11-03 15:52:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Add your code here
+
+
+function submitData(name, email){
+    const configUser = {
+        method : "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name : name,
+            email : email
+        })
+    }
+
+   return fetch("http://localhost:3000/users", configUser)
+    .then((res) => res.json())
+    .then((obj) => {
+        let idParagraph = document.createElement('p')
+        idParagraph.innerHTML = obj.id
+        document.querySelector("body").appendChild(idParagraph)
+    })
+    .catch(function(error){
+        let errParagraph = document.createElement('p');
+        errParagraph.textContent = error.message;
+        document.querySelector("body").appendChild(errParagraph)
+    })
+   
+}
+
+
+
+
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//     },
+//     // body: JSON.stringify({
+//     //     dogName: "Byron",
+//     //     dogBreed: "Poodle",
+//     //   }),
+//     //OR you can use this:
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
